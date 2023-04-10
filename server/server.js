1c8d4dca444d6747b3b4d3881da4f302da4c9c70
christasimaan <cesimaan@seas.upenn.edu>
2023-04-10 19:50:19 -0400

    routes

--- server/server.js
+++ server/server.js
@@ -10,15 +10,13 @@ app.use(cors({
 
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
-app.get('/author/:type', routes.author);
-app.get('/random', routes.random);
-app.get('/song/:song_id', routes.song);
-app.get('/album/:album_id', routes.album);
-app.get('/albums', routes.albums);
-app.get('/album_songs/:album_id', routes.album_songs);
-app.get('/top_songs', routes.top_songs);
-app.get('/top_albums', routes.top_albums);
-app.get('/search_songs', routes.search_songs);
+app.get('/home', routes.home);
+app.push('/signup', routes.signup);
+app.get('/signup/:login', routes.signup_login);
+app.get('/search', routes.search);
+app.get('/search/:recipeid', routes.search_rec);
+app.get('/likes/:username', routes.likes_user);
+app.push('/likes', routes.likes);
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
