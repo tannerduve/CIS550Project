835d355d4418d6fac34cc4905c8ac865ce858c08
christasimaan <cesimaan@seas.upenn.edu>
2023-04-20 19:24:35 -0400

    syntax recipecard

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -29,7 +29,7 @@ export default function RecipeCard({ recipeId, handleClose }) {
     //       .then(res => res.json())
     //       .then(resJson => set state variable with album data)
     //     })
-    fetch(`http://${config.server_host}:${config.server_port}/recipe/${RecipeId}`)
+    fetch(`http://${config.server_host}:${config.server_port}/recipe/${recipeId}`)
       .then(res => res.json())
       .then(resJson => {
         setRecipeData(resJson)
@@ -41,9 +41,9 @@ export default function RecipeCard({ recipeId, handleClose }) {
   }, [recipeId]);
 
   const chartData = [
-    { name: 'Danceability', value: songData.danceability },
-    { name: 'Energy', value: songData.energy },
-    { name: 'Valence', value: songData.valence },
+    { name: 'Danceability', value: recipeData.danceability },
+    { name: 'Energy', value: recipeData.energy },
+    { name: 'Valence', value: recipeData.valence },
   ];
 
   const handleGraphChange = () => {
@@ -60,13 +60,13 @@ export default function RecipeCard({ recipeId, handleClose }) {
         p={3}
         style={{ background: 'white', borderRadius: '16px', border: '2px solid #000', width: 600 }}
       >
-        <h1>{songData.title}</h1>
+        <h1>{recipeData.title}</h1>
         <h2>Album:&nbsp;
-          <NavLink to={`/albums/${albumData.album_id}`}>{albumData.title}</NavLink>
+          <NavLink to={`/albums/${authorData.album_id}`}>{authorData.title}</NavLink>
         </h2>
-        <p>Duration: {formatDuration(songData.duration)}</p>
-        <p>Tempo: {songData.tempo} bpm</p>
-        <p>Key: {songData.key_mode}</p>
+        <p>Duration: {formatDuration(recipeData.duration)}</p>
+        <p>Tempo: {recipeData.tempo} bpm</p>
+        <p>Key: {recipeData.key_mode}</p>
         <ButtonGroup>
           <Button disabled={barRadar} onClick={handleGraphChange}>Bar</Button>
           <Button disabled={!barRadar} onClick={handleGraphChange}>Radar</Button>
