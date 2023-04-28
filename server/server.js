3653b092b0dfcc984b42ff993cc86c8975a940ca
ry-boyle <ryboyle@seas.upenn.edu>
2023-04-28 16:06:04 -0400

    syntax fix

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.get('/author_reviews/:AuthorId', routes.author_reviews);
 
 
 app.listen(config.server_port, () => {
-  console.log(`Server running at http://${config.server_host}:${config.server_port}/login`)
+  console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
 });
 
 module.exports = app;
