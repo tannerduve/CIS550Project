4ef8854313a157e9f488cca233c8507cbe86f10f
ry-boyle <ryboyle@seas.upenn.edu>
2023-04-28 21:01:19 -0400

    logout button

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,6 @@
-import { AppBar, Container, Toolbar, Typography } from '@mui/material'
+import { AppBar, Container, Toolbar, Typography, Button } from '@mui/material'
 import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 // The hyperlinks in the NavBar contain a lot of repeated formatting code so a
 // helper component NavText local to the file is defined to prevent repeated code.
@@ -28,10 +29,17 @@ const NavText = ({ href, text, isMain }) => {
   )
 }
 
+
 // Here, we define the NavBar. Note that we heavily leverage MUI components
 // to make the component look nice. Feel free to try changing the formatting
 // props to how it changes the look of the component.
 export default function NavBar() {
+  const navigate = useNavigate();
+
+  const logout = () => {
+  window.sessionStorage.clear();
+  navigate('/');
+}
   return (
     <AppBar position='static'>
       <Container maxWidth='xl'>
@@ -42,6 +50,10 @@ export default function NavBar() {
           <NavText href='/reviews' text='REVIEWS'/>
           <NavText href='/likes' text='LIKES' />
           <NavText href='/' text='LOGIN'/>
+
+          <Button variant="contained" color="secondary"  onClick={() => logout() } >
+            LOGOUT
+          </Button>
         </Toolbar>
       </Container>
     </AppBar>
